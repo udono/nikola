ba8d2086a1fef3d27b89787a3b0dff5607295cc0
Udo Spallek <udono@gmx.net>
2013-10-25 12:16:53 +0000

    Fix current lang api

--- nikola/plugins/compile/rest/post_list.py
+++ nikola/plugins/compile/rest/post_list.py
@@ -30,6 +30,7 @@ import uuid
 from docutils import nodes
 from docutils.parsers.rst import Directive, directives
 
+from nikola import utils
 from nikola.plugin_categories import RestExtension
 
 
@@ -117,7 +118,7 @@ class PostList(Directive):
         slugs = self.options.get('slugs')
         slugs = [s.strip() for s in slugs.split(',')] if slugs else []
         show_all = self.options.get('all', False)
-        lang = self.options.get('lang', self.site.current_lang())
+        lang = self.options.get('lang', utils.LocaleBorg().current_lang)
         template = self.options.get('template', 'post_list_directive.tmpl')
         post_list_id = self.options.get('id', 'post_list_' + uuid.uuid4().hex)
 
